cc1ba32cd03e67cd4702a807b2c012aaf8328bf5
chaeyun-sim <bysimune@gmail.com>
2023-01-12 23:15:15 +0900

    updated program logic

--- imageResizer/renderer/js/renderer.js
+++ imageResizer/renderer/js/renderer.js
@@ -1,24 +1,35 @@
-// Some JavaScript to load the image and show the form. There is no actual backend functionality. This is just the UI
-
 const form = document.querySelector('#img-form');
+const img = document.querySelector('#img');
+const outputPath = document.querySelector('#output-path');
+const filename = document.querySelector('#filename');
+const heightInput = document.querySelector('#height');
+const widthInput = document.querySelector('#width');
 
-function loadImage(e) {
-  const file = e.target.files[0];
+
+const loadImage = (event) => {
+  const file = event.target.files[0];
 
   if (!isFileImage(file)) {
-      alert('Please select an image file');
-        return;
+    console.log('Please select an image.')
+    return;
+  };
+
+  // Get original dimensions
+  const image = new Image();
+  image.src = URL.createObjectURL(file);
+  image.onload = function () {
+    widthInput.value = this.width;
+    heightInput.value = this.height;
   }
 
   form.style.display = 'block';
-  document.querySelector(
-    '#filename'
-  ).innerHTML = file.name;
+  filename.innerText = file.name;
 }
 
-function isFileImage(file) {
-    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-    return file && acceptedImageTypes.includes(file['type'])
+// Make Sure file is image
+const isFileImage = (file) => {
+  const acceptedImageTypes = ['image/gif', 'image/png', 'image/jpeg', 'image/jpg'];
+  return file && acceptedImageTypes.includes(file['type']);
 }
 
-document.querySelector('#img').addEventListener('change', loadImage);
+img.addEventListener('change', loadImage);
